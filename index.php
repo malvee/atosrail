df0f855f7a88cb66583c3fa7f8f48a907ce40158
Momshad Alvee <momshadalvee@gmail.com>
2015-02-09 16:18:26 -0800

    connected to online database

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <html>
-	hello
+<?php
+	 $host = "eu-cdbr-azure-north-b.cloudapp.net";
+    $user = "b1ab8a4c6aa690";
+    $pwd = "efd91e32";
+    $db = "atosraiAaM0G4XAp";
+    try {
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e){
+        die(var_dump($e));
+    }
+    $sql_select = "SELECT * FROM users";
+    $stmt = $conn->query($sql_select);
+    $registrants = $stmt->fetchAll(); 
+    foreach($registrants as $registrant)
+    {
+    	echo $registrant["query"];
+    }
+
+?>
+
 </html>
