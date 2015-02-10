ba9d6f41d10d12e3fae7fcfe3aaf1f2bd625d8ab
Momshad Alvee <momshadalvee@gmail.com>
2015-02-09 16:39:56 -0800

    uploading project

--- index.php
+++ index.php
@@ -1,24 +1,4 @@
 <html>
-<?php
-	 $host = "eu-cdbr-azure-north-b.cloudapp.net";
-    $user = "b1ab8a4c6aa690";
-    $pwd = "efd91e32";
-    $db = "atosraiAaM0G4XAp";
-    try {
-        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    }
-    catch(Exception $e){
-        die(var_dump($e));
-    }
-    $sql_select = "SELECT * FROM users";
-    $stmt = $conn->query($sql_select);
-    $registrants = $stmt->fetchAll(); 
-    foreach($registrants as $registrant)
-    {
-    	echo $registrant["query"];
-    }
-
-?>
+<a href="atosrail.azurewebsites.com/cis-ucl-social-media">Click Here</a>
 
 </html>
