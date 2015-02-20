2f16e498feb3e46903caf4bca483362256a11133
Chaitanya Agrawal <chaitanya.agrawal.13@ucl.ac.uk>
2015-02-20 13:57:59 +0000

    Changes to Front End
    * Made good,bad,neutral,all pages look like login.php
    * Uncommented login session in settings.php

--- cis-ucl-social-media/warning.php
+++ cis-ucl-social-media/warning.php
@@ -11,29 +11,33 @@ if ($_SESSION["loggedIn"] ==1)
 	<meta name = "viewport" content = "width= device-width, initial-scale=1.0">
 	<link href  = "../css/bootstrap.min.css" rel = "stylesheet">
 	<link href  = "../css/styles.css" rel = "stylesheet">
+		<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script src="../js/bootstrap.js"></script>
 	</head>
 	<body>
 			
 			<div class = "navbar navbar-default navbar-static-top">
-			<div class = "container">
 
-					<a href="settings.php" class = "navbar-brand" >
-					<img class = "navbar-brand"   style="padding: 0.5em;width: 4em; height: 3.9em; float: left; margin-top: -1.25em" src="menuconreal.png">
-				</a>
+		<div class ="container">
 
-					<ul class = "nav navbar-nav navbar-right">
-						<li><a href = "index.php">Log Out</a></li>
-					</ul>
-				
-
-
-			</div>
 			
-		</div>
+					<a href="settings.php" class = "navbar-brand" >
+					<img class = "navbar-brand"   style="padding: 0.5em;width: 3.8em; height: 3.8em; float: left; margin-top: -1.25em" src="menuconreal.png">
+					</a>
+
+					<div class = "collapse navbar-collapse navHeaderCollapse">
+					<ul class = "nav navbar-nav navbar-right">
+						<li><a href = "index.php">LOG OUT</a></li>
+					</ul>
+				</div>
+
+				</div></div>
+
 
 
 			<div class = "container">
 				<div class = "row">
+					<br>
 					<div class = "col-md-3">
 						<a href="bad.php" class = "btn btn-danger btn-block">Bad</a>
 					</div>
@@ -88,17 +92,17 @@ if ($_SESSION["loggedIn"] ==1)
 		
 			
 			
+<br><br><br><br>
 
-		</div>
-		<div class = "navbar navbar-inverse navbar-fixed-bottom">
+			
+		<div class = "navbar navbar-default navbar-fixed-bottom" >
 			<div class = "container">
 				<p class = "navbar-text pull-left">2014 Developed by UCL ATOS Team 4<br>All Rights Reserved</p>
 			</div>
 		</div>
 
 
-		<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-		<script src="../js/bootstrap.js"></script>
+	
 		<?php }
 		else
 		{
